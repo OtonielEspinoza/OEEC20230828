706a75b58635e09a5dba39b61ac4c50711e399b7
Otoniel Esteban Espinoza Contreras <esteban15espinosa@outlook.com>
2023-08-28 08:13:41 -0600

    inicializacion del proyecto

---
+++ OEEC20230828/src/Entidad/Empleados.java
@@ -0,0 +1,87 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Entidad;
+
+/**
+ *
+ * @author H
+ */
+public class Empleados {
+    
+    public class Empleado {
+    private int id;
+    private String nombre;
+    private String apellido;
+    private String correoElectronico;
+    private String puesto;
+
+    // Constructores
+    public Empleado() {
+    }
+
+    public Empleado(int id, String nombre, String apellido, String correoElectronico, String puesto) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correoElectronico = correoElectronico;
+        this.puesto = puesto;
+    }
+
+    // Métodos getter y setter
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    public String getPuesto() {
+        return puesto;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puesto = puesto;
+    }
+
+    // Método toString para representación legible
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", correoElectronico='" + correoElectronico + '\'' +
+                ", puesto='" + puesto + '\'' +
+                '}';
+    }
+}
+
+    
+}
